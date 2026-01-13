0dfbaf131a037637a2d35f43980e67557b3c726a
MattEllingson-Kirkwood <matt.ellingson@kirkwood.edu>
2026-01-13 09:21:36 -0600

    Added ino file

---
+++ 01Blink.ino.ino
@@ -0,0 +1,15 @@
+/*Blink LED Sketch 
+by Matt Ellingson 1-12-2026*/
+
+void setup() {
+  pinMode(13, OUTPUT); //set digital pin 13 to output
+
+}
+
+void loop() {
+  digitalWrite(13, HIGH); // turn on digital pin 13
+  delay(250); //pause for .25 second
+  digitalWrite(13, LOW); // turn off digital pin 13
+  delay(250); //pause for .25 second
+
+}
